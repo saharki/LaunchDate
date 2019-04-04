7095aaee912fb8bca7ef36e4d745e89682d80d99
mishaled <mishaled@gmail.com>
2019-04-05 01:12:15 +0300

    - implement router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import Chats from './Chats';
 import MatchFinder from './MatchFinder';
 import { chooseGroup } from './actions';
 import { connect } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 
 const containerStyle = {
   height: '812px',
@@ -13,8 +17,26 @@ const containerStyle = {
   position: 'relative'
 };
 
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+});
+
 const App = (props) => {
+  const [status, setStatus] = useState('pending');
+  const [currentRestaraunts, setCurrentRestaraunts] = useState([]);
   const [chosenRestaraunt, setChosenRestaraunt] = useState(null);
+  const { classes } = props;
+
+  useEffect(() => {
+    axios.get('https://515fdb63.ngrok.io/restaurants')
+      .then((result) => {
+        setCurrentRestaraunts(result.data);
+        setStatus('match');
+      });
+  }, []);
+
 
   useEffect(() => {
     if (chosenRestaraunt && chosenRestaraunt.groups && chosenRestaraunt.groups.length > 0) {
@@ -25,18 +47,29 @@ const App = (props) => {
 
   return <div style={containerStyle}>
     {
-      chosenRestaraunt ?
-        <Chats /> :
-        <MatchFinder setChosenRestaraunt={(restaraunt) => {
-          setChosenRestaraunt(restaraunt);
-        }}
-        />
+      status === 'pending' ?
+        <CircularProgress className={classes.progress} /> :
+        status === 'match' && currentRestaraunts.length > 0 ?
+          <MatchFinder
+            restaraunt={currentRestaraunts[0]}
+            setChosenRestaraunt={(restaraunt) => {
+              setChosenRestaraunt(restaraunt);
+            }}
+            removeRestaraunt={(restaraunt) => {
+              setCurrentRestaraunts(props.currentRestaraunts.slice(1, props.currentRestaraunts.length - 1));
+            }}
+          /> :
+          <Chats />
     }
   </div>
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 const mapStateToProps = function ({ chosenRestaraunt }) {
   return {};
 };
 
-export default connect(mapStateToProps)(App);
+export default withStyles(styles)(connect(mapStateToProps)(App));
--- client/src/MatchFinder.jsx
+++ client/src/MatchFinder.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import ThumbUp from '@material-ui/icons/ThumbUp';
 import ThumbDown from '@material-ui/icons/ThumbDown';
-import axios from 'axios'
 
 const Container = styled.div`
     display: flex;
@@ -39,36 +38,18 @@ const RestarauntOption = (props) => {
 }
 
 const MatchFinder = (props) => {
-    const [currentRestaraunts, setCurrentRestaraunts] = useState([]);
-
-    useEffect(() => {
-        axios.get('https://515fdb63.ngrok.io/restaurants')
-            .then((result) => {
-                setCurrentRestaraunts(result.data);
-            });
-    }, []);
-
     return (
         <Container>
-            {
-                currentRestaraunts.length > 0 ?
-                    (
-                        <div>
-                            <RestarauntOption restaraunt={currentRestaraunts[0]} />
-                            <Actions>
-                                <ThumbUp onClick={() => {
-                                    const chosenRestaraunt = currentRestaraunts[0];
-                                    props.setChosenRestaraunt(chosenRestaraunt);
-                                }} />
-                                <ThumbDown onClick={() => {
-                                    setCurrentRestaraunts(currentRestaraunts.slice(1, currentRestaraunts.length - 1));
-                                }} />
-                            </Actions>
-                        </div>
-                    ) :
-                    <div>Sorry, no restaraunts to show!</div>
-            }
-        </Container>
+            <RestarauntOption restaraunt={props.restaraunt} />
+            <Actions>
+                <ThumbUp onClick={() => {
+                    props.setChosenRestaraunt(props.restaraunt);
+                }} />
+                <ThumbDown onClick={() => {
+                    props.removeRestaraunt(props.restaraunt);
+                }} />
+            </Actions>
+        </Container >
     );
 }
 
