be03ca4585b41957445cfa1205ccde6f40ebe4e6
Orel Balilti <orelba93@gmail.com>
2019-04-05 09:48:07 +0300

    endpoint changed

---
+++ vue-dist/precache-manifest.08b00ebb5b6a669b91016c5edb4d51fd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0f62f1fb47129ab96a2317d105e4635",
+    "url": "/pics/asian.jpeg"
+  },
+  {
+    "revision": "c71f48a423d2d590f9e965a5562e9719",
+    "url": "/pics/italian.jpeg"
+  },
+  {
+    "revision": "31494bc960c9c3d56d1d85444e024790",
+    "url": "/pics/mexican.jpeg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f900ee1534063bbd67131a661089006d",
+    "url": "/pics/meat.jpeg"
+  },
+  {
+    "revision": "4909ccec2e070ee30c1021697b0c7ea7",
+    "url": "/pics/logo.jpg"
+  },
+  {
+    "revision": "4c56532dce6aecee3eca91f7caa92818",
+    "url": "/pics/healthy.jpeg"
+  },
+  {
+    "revision": "d26856b5ca5c124e2a92",
+    "url": "/js/chunk-vendors.fb43acd8.js"
+  },
+  {
+    "revision": "b08415bdfbc0cf8fdad4",
+    "url": "/js/app.93936723.js"
+  },
+  {
+    "revision": "8ef5c8147da0fce07b66",
+    "url": "/js/about.422beb19.js"
+  },
+  {
+    "revision": "33eb2fbb8e2756f3b16feeaafbb90420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d26856b5ca5c124e2a92",
+    "url": "/css/chunk-vendors.0126acfc.css"
+  },
+  {
+    "revision": "b08415bdfbc0cf8fdad4",
+    "url": "/css/app.00f465c9.css"
+  }
+];
